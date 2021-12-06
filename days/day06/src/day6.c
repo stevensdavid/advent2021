1fefd539d7f28023fa60a43912c26734ca523014
David Stevens <david@hejare.se>
2021-12-06 16:49:29 +0100

    Solve day 6 in C

---
+++ days/day06/src/day6.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char COMMA = 0x2C;
+
+int main() {
+  // Parse input. ASSUMES ONLY SINGLE DIGITS AND COMMAS IN STDIN
+  long state[9] = {0};
+  char c = getchar();
+  while (c != EOF) {
+    // If it's a comma, we skip
+    if (c != COMMA) {
+      int val = atoi(&c);
+      state[val] += 1;
+    }
+    c = getchar();
+  }
+  for (size_t i = 0; i < 9; i++) {
+    printf("%ld,", state[i]);
+  }
+  printf("\n");
+  // Simulate
+  int n_days = 256;
+  for (int day = 0; day < n_days; day++) {
+    long new_state[9];
+    for (int time = 8; time > 0; time--) {
+      new_state[time - 1] = state[time];
+    }
+    new_state[6] += state[0];
+    // New births
+    new_state[8] = state[0];
+
+    // Transfer new state
+    for (int idx = 0; idx < 9; idx++) {
+      state[idx] = new_state[idx];
+    }
+  }
+  // Sum number of fish after simulation ends
+  long sum = 0;
+  for (int idx = 0; idx < 9; idx++) {
+    sum += state[idx];
+  }
+  printf("The final number of fish is: %ld\n", sum);
+}
